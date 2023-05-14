3596cf356ebe95be4a7b951f76a65ba510cebcf2
Krippykesh <khrithikesh45@gmail.com>
2023-05-14 17:00:48 +0530

    Added Folder Structure and created a baseline

---
+++ Components/FavouritesItem/index.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export  function FavouritesItem() {
+  return (
+    <View>
+      <Text>FavouritesItem</Text>
+    </View>
+  );
+}
+ 
---
+++ Components/ProductDetailsItem/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export  function ProductDetailsItem() {
+  return (
+    <View>
+      <Text>Product Details Item</Text>
+    </View>
+  );
+}
